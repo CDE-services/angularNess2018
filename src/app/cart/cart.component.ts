60c7164470380a64dd024dc7083302744bbed066
Michaela Bačíková <michaela.bacikova@tuke.sk>
2018-12-01 16:27:23 +0100

    lab 9 - zabudnuty cart komponent

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  template: '<div class="container"><h2>This is my cart!</h2></div>'
+})
+export class CartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
